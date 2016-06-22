09c93f4cd42ba236a4de9a5c6e4d7f3e981052ee
Vu Dinh <isthis.vu@gmail.com>
2016-06-22 11:35:03 -0700

    created Message and Comment models

--- apps/wall/models.py
+++ apps/wall/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
-
+from ..login_reg.models import User
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+	message = models.TextField()
+	wall = models.ForeignKey(User, related_name = "wall")
+	user = models.ForeignKey(User, related_name = "user")
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
+	def __str__(self):
+		return self.message
+	class Meta:
+		db_table = "messages"
+
+class Comment(models.Model):
+	comment = models.TextField()
+	user = models.ForeignKey(User)
+	message = models.ForeignKey(Message)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return self.comment
+	class Meta:
+		db_table = "comments"
